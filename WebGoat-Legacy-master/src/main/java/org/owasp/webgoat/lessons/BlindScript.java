9d2c2536b93cd190f4d54017506a652dcdf11d7b
bharatNakka <nbarath2009@gmail.com>
2023-06-26 17:27:38 +0530

    Update BlindScript.java
    LineStart: 162(159,160)
    FileName: BlindScript.java
    TargetFunction: java.io.File.File()

--- WebGoat-Legacy-master/src/main/java/org/owasp/webgoat/lessons/BlindScript.java
+++ WebGoat-Legacy-master/src/main/java/org/owasp/webgoat/lessons/BlindScript.java
@@ -157,8 +157,8 @@ public class BlindScript extends LessonAdapter
     public void executeSpyWare( WebSession s )
     {
         // Lets gather some information about the users browsing history
-        String userHome = System.getProperty("user.home" ) + "\\Local Settings\\Temporary Internet Files";
-        String separator = System.getProperty("line.separator");
+       // String userHome = System.getProperty("user.home" ) + "\\Local Settings\\Temporary Internet Files";
+        //String separator = System.getProperty("line.separator");
         File dir = new File(userHome);
         StringBuffer browserFiles = new StringBuffer();
         if (dir.isDirectory()) {
